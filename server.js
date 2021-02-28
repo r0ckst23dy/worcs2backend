479591ed8136bb1568c1db24234b8cf46304ebc5
r0ckst34dy <r0ck1ngst34dy@outlook.com>
2021-02-28 00:07:53 -0500

    still refactoring

--- server.js
+++ server.js
@@ -1,19 +1,115 @@
+// const express =  require('express');
+// const app = express();
+// const cors = require('cors');
+// const mongoose = require('mongoose');
+// const bodyParser= require('body-parser');
+// const port = process.env.PORT || 8080;
+// const ordersRoutes = express.Router();
+// const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+
+// const uri = "mongodb+srv://Joshua:12345josh@cluster0.fxm00.mongodb.net/WORCS?retryWrites=true&w=majority";
+
+
+// let Orders = require('./orders.model');
+// const { urlencoded } = require('body-parser');
+
+// app.use(cors());
+// app.use(bodyParser.json());
+
+// mongoose.connect(uri, {useUnifiedTopology: true, useNewUrlParser: true})
+//     .then(() => {
+//         console.log('Connected to Database');
+//     })
+//     .catch( (err) => {
+//         console.log((`Error connecting to database. \n${err}`));
+//     })
+
+
+// ordersRoutes.route('/').get(function(req, res) {
+//     Orders.find(function(err, orders) { 
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             res.json(orders)
+//         }
+//     });
+// });
+
+// ordersRoutes.route('/:id').get(function(req, res) {
+//     let id = req.params.id;
+//     Orders.findById(id, function(err, orders) {
+//         res.json(orders);
+//     });
+// });
+
+// ordersRoutes.route('/add').post(function(req, res)
+// { 
+//     let orders = new Orders(req.body);
+//     orders.save()
+//         .then(orders => {
+//             res.status(200).json({'orders': 'work order added successfully'});
+//         })
+//         .catch(err => { 
+//             res.status(400).send('adding new work order failed');
+//         });
+// });
+
+// ordersRoutes.route('/update/:id').post(function(req, res) { 
+//     Orders.findById(req.params.id, function(err, orders) {
+//         if (!orders)
+//             res.status(404).send('data is not found');
+//         else
+//             orders.titie = req.body.title;
+//             orders.requesDate = req.body.requesDate;
+//             orders.assignedDate = req.body.assignedDate;
+//             orders.completionDate = req.body.completionDate;
+//             orders.requestedBy = req.body.requestBy;
+//             orders.completedBy = req.body.completeBy;
+//             orders.assignedTo = req.body.assignedTo;
+//             orders.priority = req.body.priority;
+//             orders.assignmentSummary = req.body.assignmentSummary;
+//             orders.completionSummary = req.body.completionSummary;
+//             orders.summary = req.body.summary;
+//             orders.assignedImage = req.body.assignedImage;
+//             orders.completedImage = req.body.completedImage;
+//             orders.current = req.body.current;
+//             orders.assigned = req.body.assigned;
+//             orders.completed = req.body.completed;
+//             orders.reviewed = req.body.reviewed;
+
+//             orders.save().then(orders => {
+//                 res.json('Work Order Updated')
+//             })
+//             .catch(err => {
+//                 res.status(400).send('Update not possible')
+//             });
+
+//     });
+// });
+
+
+// app.use('/orders', ordersRoutes)
+
+// app.listen(port, function() { 
+//     console.log("Server is running on Port:" + port);
+// })
+
+  
 const express =  require('express');
-const cors = require('cors');
-const helmet = require('helmet')
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser= require('body-parser');
+const cors = require('cors');
 const port = process.env.PORT || 8080;
 const ordersRoutes = express.Router();
 
 const uri = "mongodb+srv://Joshua:12345josh@cluster0.fxm00.mongodb.net/WORCS?retryWrites=true&w=majority";
 
+
 let Orders = require('./orders.model');
 
-app.use(helmet());
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: true});
 app.use(bodyParser.json());
 
 mongoose.connect(uri, {useUnifiedTopology: true, useNewUrlParser: true})
@@ -42,9 +138,7 @@ ordersRoutes.route('/:id').get(function(req, res) {
     });
 });
 
-// ordersRoutes.route('/add').post(function(req, res)
-ordersRoutes.post('/add', urlencodedParser, (req, res) => {
-
+ordersRoutes.route('/add').post(function(req, res) { 
     let orders = new Orders(req.body);
     orders.save()
         .then(orders => {
@@ -53,7 +147,6 @@ ordersRoutes.post('/add', urlencodedParser, (req, res) => {
         .catch(err => { 
             res.status(400).send('adding new work order failed');
         });
-        
 });
 
 ordersRoutes.route('/update/:id').post(function(req, res) { 
